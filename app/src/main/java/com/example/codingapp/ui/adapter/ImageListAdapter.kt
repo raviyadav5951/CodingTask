1a014a791f5f935befbaa2f852b6feccfc16e914
Ravi nntx <raviyadav5951@gmail.com>
2020-09-23 14:31:43 +0530

    displayed loader when search api gets called, displayed circular progress in list item to show loading progress

--- app/src/main/java/com/example/codingapp/ui/adapter/ImageListAdapter.kt
+++ app/src/main/java/com/example/codingapp/ui/adapter/ImageListAdapter.kt
@@ -2,16 +2,13 @@ package com.example.codingapp.ui.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.codingapp.R
 import com.example.codingapp.databinding.ItemImagesBinding
 import com.example.codingapp.model.Images
-import com.example.codingapp.util.getProgressDrawable
 import com.example.codingapp.util.loadImage
 
-class ImageListAdapter(private val imageList:ArrayList<Images>):RecyclerView.Adapter<ImageListAdapter.ImageViewHolder>() {
+class ImageListAdapter(private val imageList:ArrayList<Images>,val itemClickListener: OnItemClickListener):RecyclerView.Adapter<ImageListAdapter.ImageViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
 
@@ -22,12 +19,15 @@ class ImageListAdapter(private val imageList:ArrayList<Images>):RecyclerView.Ada
     override fun getItemCount()=imageList.size
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        Log.e("tag","onBindViewHolder")
         val image:Images=imageList[position]
         holder.bind(image)
+        holder.itemView.setOnClickListener {
+            itemClickListener.onItemClicked(image)
+        }
     }
 
     fun updateImageList(newAnimalList:List<Images>){
-        Log.e("tag","iupdateImageLists")
         imageList.clear()
         imageList.addAll(newAnimalList)
         Log.e("tag","iupdateImageLists size=${imageList.size}")
@@ -36,10 +36,11 @@ class ImageListAdapter(private val imageList:ArrayList<Images>):RecyclerView.Ada
 
     class ImageViewHolder(private val itemBinding: ItemImagesBinding):RecyclerView.ViewHolder(itemBinding.root){
         fun bind(image: Images) {
-
             itemBinding.imgurImage.loadImage(itemBinding.imgurImage,image.link)
-
         }
+    }
 
+    interface OnItemClickListener{
+        fun onItemClicked(image: Images)
     }
 }
